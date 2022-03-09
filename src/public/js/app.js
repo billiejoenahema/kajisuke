3bc880a69f1842a51de52104bcd5a71cf2930c3e
billiejoenahema <billiejoenahema@gmail.com>
2022-03-09 21:22:25 +0900

    install: simpledotcss

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -36948,10 +36948,10 @@ var __WEBPACK_AMD_DEFINE_RESULT__;/**
 
 /***/ }),
 
-/***/ "./resources/css/app.css":
-/*!*******************************!*\
-  !*** ./resources/css/app.css ***!
-  \*******************************/
+/***/ "./resources/sass/app.scss":
+/*!*********************************!*\
+  !*** ./resources/sass/app.scss ***!
+  \*********************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
@@ -42654,7 +42654,7 @@ var index = {
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
 /******/ 	__webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/js/app.js")))
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/css/app.css")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/sass/app.scss")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -1,5 +1,4 @@
-const mix = require("laravel-mix");
-
+const mix = require('laravel-mix');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,6 +10,7 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.js("resources/js/app.js", "public/js")
-    .vue()
-    .postCss("resources/css/app.css", "public/css", []);
+mix
+  .js('resources/js/app.js', 'public/js')
+  .vue()
+  .sass('resources/sass/app.scss', 'public/css');
