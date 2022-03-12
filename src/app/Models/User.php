b7b146e263f7ef15911e00629dc91e8c4cb2c6e4
billiejoenahema <billiejoenahema@gmail.com>
2022-03-12 22:12:07 +0900

    add: relationship to user model

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * 紐づく家事を取得する
+     */
+    public function houseworks()
+    {
+        return $this->hasMany(Housework::class);
+    }
 }
