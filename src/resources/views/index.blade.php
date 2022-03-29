708fae2d2adb322754dc33cfb48cfdbeed93b7cf
billiejoenahema <billiejoenahema@gmail.com>
2022-03-29 21:17:04 +0900

    make: app.js, index.blade.php to implement Vue

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>{{ config('app.name') }}</title>
+    <!-- Fonts -->
+    <link
+      href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"
+      rel="stylesheet"
+    />
+    <!-- Styles -->
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+  </head>
+  <body>
+    <noscript>
+      <strong
+        >We're sorry but <%= htmlWebpackPlugin.options.title %> doesn't work
+        properly without JavaScript enabled. Please enable it to
+        continue.</strong
+      >
+    </noscript>
+    <div id="app"></div>
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+  </body>
+</html>
