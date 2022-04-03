7f230ffcee450a504196848bdb8f611f2039b04e
billiejoenahema <billiejoenahema@gmail.com>
2022-04-03 18:42:52 +0900

    refact: move navigation bar from App.vue to main page component

--- src/resources/js/App.vue
+++ src/resources/js/App.vue
@@ -1,25 +1,6 @@
-<script setup>
-import { computed, onMounted } from 'vue';
-import { useRouter } from 'vue-router';
-import { useStore } from 'vuex';
-import NavigationBar from './components/NavigationBar';
-
-const router = useRouter();
-const store = useStore();
-
-const profile = computed(() => store.getters['profile/profile']);
-
-onMounted(async () => {
-  await store.dispatch('profile/get');
-  const isLogin = computed(() => store.getters['profile/isLogin']);
-  if (!isLogin.value) {
-    router.push('/login');
-  }
-});
-</script>
+<script setup></script>
 
 <template>
-  <NavigationBar :userName="profile.name" />
   <main>
     <router-view />
   </main>
--- src/resources/js/pages/MainPage.vue
+++ src/resources/js/pages/MainPage.vue
@@ -1,3 +1,23 @@
-<script setup></script>
+<script setup>
+import { computed, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
+import NavigationBar from '../components/NavigationBar';
 
-<template></template>
+const router = useRouter();
+const store = useStore();
+
+const profile = computed(() => store.getters['profile/profile']);
+
+onMounted(async () => {
+  await store.dispatch('profile/get');
+  const isLogin = computed(() => store.getters['profile/isLogin']);
+  if (!isLogin.value) {
+    router.push('/login');
+  }
+});
+</script>
+
+<template>
+  <NavigationBar :userName="profile.name" />
+</template>
