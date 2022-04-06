4aebeebde55b8f0e06f124f9b1a005a52a12f7f5
billiejoenahema <billiejoenahema@gmail.com>
2022-04-06 20:45:45 +0900

    feat: loading spinner when fetching data

--- src/resources/js/pages/MainPage.vue
+++ src/resources/js/pages/MainPage.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { computed, onMounted } from 'vue';
+import { computed, onMounted, ref } from 'vue';
+import VueElementLoading from 'vue-element-loading';
 import { useRouter } from 'vue-router';
 import { useStore } from 'vuex';
 import NavigationBar from '../components/NavigationBar';
@@ -8,20 +9,32 @@ const router = useRouter();
 const store = useStore();
 
 const profile = computed(() => store.getters['profile/profile']);
+const isLogin = computed(() => store.getters['profile/isLogin']);
+const isLoading = ref(false);
+const houseworks = computed(() => store.getters['housework/data']);
 
 onMounted(async () => {
   await store.dispatch('profile/get');
-  await store.dispatch('housework/get');
-  const isLogin = computed(() => store.getters['profile/isLogin']);
   if (!isLogin.value) {
     router.push('/login');
+  } else {
+    isLoading.value = true;
+    await store.dispatch('housework/get');
+    isLoading.value = false;
   }
 });
-const houseworks = computed(() => store.getters['housework/data']);
 </script>
 
 <template>
   <NavigationBar :userName="profile.name" />
+  <vue-element-loading
+    v-if="isLogin"
+    :active="isLoading"
+    color="#fff"
+    background-color="rgba(0, 0, 0, 0)"
+    spinner="spinner"
+    is-full-screen
+  />
   <table>
     <thead>
       <tr>
@@ -32,7 +45,7 @@ const houseworks = computed(() => store.getters['housework/data']);
     <tbody>
       <tr v-for="item in houseworks" :key="item.id">
         <td>{{ item.title }}</td>
-        <td>with two columns</td>
+        <td>{{ item.comment }}</td>
       </tr>
     </tbody>
   </table>
