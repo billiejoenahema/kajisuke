6a4f1eadbd78f181df4f47332c16513507498db0
billiejoenahema <billiejoenahema@gmail.com>
2022-04-14 21:00:48 +0900

    feat: update housework order frontend api

--- src/resources/js/store/index.js
+++ src/resources/js/store/index.js
@@ -3,12 +3,14 @@ import { createStore } from 'vuex';
 import auth from './modules/auth';
 import category from './modules/category';
 import housework from './modules/housework';
+import houseworkOrder from './modules/houseworkOrder';
 import profile from './modules/profile';
 
 export const store = createStore({
   modules: {
     auth,
     category,
+    houseworkOrder,
     housework,
     profile,
   },
---
+++ src/resources/js/store/modules/houseworkOrder.js
@@ -0,0 +1,76 @@
+import axios from 'axios';
+
+const state = {
+  data: [
+    {
+      id: null,
+      user_id: 0,
+      order: '',
+    },
+  ],
+  errors: [],
+};
+
+const getters = {
+  data(state) {
+    return state.data ?? [];
+  },
+  hasErrors(state) {
+    return state.errors?.length > 0;
+  },
+  errors(state) {
+    return state.errors ?? [];
+  },
+};
+
+const actions = {
+  async get({ commit }) {
+    await axios
+      .get('/api/housework_orders')
+      .then((res) => {
+        console.log(res.status);
+        commit('resetErrors');
+        commit('setData', res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        commit('setErrors', err.message);
+        commit('setData', {});
+      });
+  },
+  async patch({ commit }, order) {
+    await axios
+      .patch('/api/housework_orders', { order })
+      .then((res) => {
+        console.log(res);
+        commit('resetErrors');
+      })
+      .catch((err) => {
+        console.log(err);
+        commit('setErrors', err.message);
+        commit('setData', {});
+      });
+  },
+};
+
+const mutations = {
+  setData(state, data) {
+    state.data = data;
+  },
+  setErrors(state, data) {
+    state.errors = [];
+    state.errors.push(data);
+  },
+  resetErrors(state) {
+    state.errors = [];
+    state.hasErrors = false;
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
