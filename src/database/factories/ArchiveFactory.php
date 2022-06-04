8010e2e5b7c38142baeb39d047d01120a09bffc7
billiejoenahema <billiejoenahema@gmail.com>
2022-06-04 15:06:57 +0900

    feat: make update archive test, factory and seeder

---
+++ src/database/factories/ArchiveFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Archive>
+ */
+class ArchiveFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'housework_id' => 1,
+            'date' => now(),
+            'content' => '履歴内容' . rand(1, 99),
+        ];
+    }
+}
---
+++ src/tests/Feature/Archive/UpdateTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature\Archive;
+
+use App\Models\Archive;
+use App\Models\Housework;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UpdateTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * 家事の履歴を更新できるか
+     *
+     * @return void
+     */
+    public function test_updateArchive()
+    {
+        /** @var \Illuminate\Contracts\Auth\Authenticatable $user */
+        $user = User::factory()->create();
+        $housework = Housework::factory()->create();
+        $archive = Archive::factory()->create([
+            'housework_id' => $housework->id,
+            'date' => now(),
+            'content' => 'テストコンテント',
+        ]);
+
+        // 実行
+        $uri = '/api/archives/' . $archive->id;
+        $response = $this->actingAs($user)->patchJson($uri , [
+            'id' => $archive->id,
+            'housework_id' => $housework->id,
+            'date' => now(),
+            'content' => '更新されたコンテント',
+        ]);
+        $response
+        ->assertOK();
+
+        $this->assertDatabaseHas('archives', [
+            'content' =>'更新されたコンテント'
+        ]);
+    }
+}
