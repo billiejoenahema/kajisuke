ae68dbb25e92c101ee90a21a730206366a1d53e3
billiejoenahema <billiejoenahema@gmail.com>
2022-07-23 15:09:03 +0900

    feat: make profile controller&resource&request

--- src/app/Http/Controllers/Api/ProfileController.php
+++ src/app/Http/Controllers/Api/ProfileController.php
@@ -3,18 +3,37 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Profile\UpdateRequest;
 use App\Http\Resources\ProfileResource;
+use App\Models\Profile;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProfileController extends Controller
 {
     /**
-     * ユーザー情報を取得する。
+     * 自身のプロフィールを取得する
+     *
      * @return ProfileResource
      */
-    public function __invoke(): ProfileResource
+    public function index(): ProfileResource
     {
         $user = Auth::user();
-        return new ProfileResource($user);
+
+        return new ProfileResource($user->profile);
+    }
+
+    /**
+     * 自身のプロフィールを更新する
+     *
+     * @param  UpdateRequest  $request
+     * @return Response
+     */
+    public function update(UpdateRequest $request): Response
+    {
+        $user = Auth::user();
+        $profile = Profile::where('user_id', $user->id)->first();
+        $profile->update($request->all());
+        return response()->json(config('const.PROFILE.UPDATED'), Response::HTTP_OK);
     }
 }
---
+++ src/app/Http/Requests/Profile/UpdateRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Profile;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule as ValidationRule;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'user_id' => 'required|integer|exists:users,id',
+            'last_name' => 'nullable|string|max:50',
+            'first_name' => 'nullable|string|max:50',
+            'gender' => ['required', 'string', ValidationRule::in(['0', '1', '2', '9'])],
+            'birth' => 'nullable|string|size:8',
+            'tel' => 'nullable|string|max:13',
+            'zipcode' => 'nullable|string|max:50',
+            'prefecture' => ['nullable', 'string'],
+            'city' => 'nullable|string|max:50',
+            'street_address' => 'nullable|string|max:50',
+        ];
+    }
+}
