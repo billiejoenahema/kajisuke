efe9dc4bebd3342cbab71a3134aed895f1dbbddc
billiejoenahema <billiejoenahema@gmail.com>
2022-07-31 21:15:44 +0900

    chore: remove vue-element-loading, npm install

--- src/resources/js/pages/MainPage.vue
+++ src/resources/js/pages/MainPage.vue
@@ -1,26 +1,16 @@
 <script setup>
-import { ref } from 'vue';
-import VueElementLoading from 'vue-element-loading';
+import { useStore } from 'vuex';
 import HouseworkList from '../components/Housework/HouseworkList';
 import NavigationBar from '../components/NavigationBar';
 import ToastMessage from '../components/ToastMessage';
 
-const isLoading = ref(false);
-const setIsLoading = (bool) => {
-  isLoading.value = bool;
-};
+const store = useStore();
+const isLoading = store.getters['loading/isLoading'];
 </script>
 
 <template>
   <ToastMessage />
-  <NavigationBar :isLoading="isLording" :setIsLoading="setIsLoading" />
-  <vue-element-loading
-    v-if="isLoading"
-    :active="isLoading"
-    color="#fff"
-    background-color="rgba(0, 0, 0, 0)"
-    spinner="spinner"
-    is-full-screen
-  />
+  <NavigationBar />
+
   <HouseworkList v-if="!isLoading" />
 </template>
